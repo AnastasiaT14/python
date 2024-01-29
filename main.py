88ab5fc1e006a4590aa8c25ffbcf25562cf01a84
unknown <anastasiatevzaia@gmail.com>
2024-01-29 21:37:50 +0400

    Changes main.py and add project1.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-sentences = []
-
-for i in range(5):
-    sentence = str(input("Enter the sentence "))
-    sentences.append(sentence)
-
-longest_string = max(sentences, key=len)
-
-for sentence in sentences:
-    print(sentence)
-print("\nThe longest sentence is:", longest_string)
+ages = {
+    "anna" : 19,
+    "dato" : 15,
+    "lika" : 24
+}
+min_value = min(ages.values())
+min_keys = [key for key in ages if ages[key]==min_value]
+print(min_keys)
