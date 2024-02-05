cdc506e754985bddba162319788f2f231dc5107a
unknown <anastasiatevzaia@gmail.com>
2024-02-05 22:31:26 +0400

    Change main.py

--- main.py
+++ main.py
@@ -1,51 +1,24 @@
-class Car:
-    def __init__(self, brand, model, production_year, color, horse_power):
-        self.brand = brand
-        self.model = model
-        self.production_year = production_year
-        self.color = color
-        self.horse_power = horse_power
-        self.is_sport_car = False
+class Student:
+    university = "Hogwarts"
 
-    def get_brand(self):
-        return self.brand
+    def __init__(self, name, grade, age):
+        self.name = name
+        self.grade = grade
+        self.age = age
 
-    def get_model(self):
-        return self.model
+    def __str__(self):
+        return "Name: {self.name}, Age: {self.age}, Grade: {self.grade}"
 
-    def get_production_year(self):
-        return self.production_year
+    @property
+    def is_passing(self):
+        return int(self.grade) > 60
 
-    def get_color(self):
-        return self.color
+    def increase_grade(self, increase):
+        self.grade = str(int(self.grade) + increase)
 
-    def get_horse_power(self):
-        return self.horse_power
 
-    def is_sporty(self):
-        return self.is_sport_car
-
-    def set_color(self, new_color):
-        if new_color != self.color:
-            self.color = new_color
-            return True
-        else:
-            return False
-
-    def set_sporty(self, is_sport_car):
-        self.is_sport_car = is_sport_car
-
-    def change_color(self, new_color):
-        if new_color != self.color:
-            self.color = new_color
-            return True
-        else:
-            return False
-
-    def increase_horse_power(self, hp):
-        if hp > 0:
-            self.horse_power += hp
-            return True
-        else:
-            return False
+student = Student("Mari", 60, 20)
+print(str(student))
+print("Is passing:", student.is_passing)
+student.increase_grade(10)
 
