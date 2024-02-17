5068a14d1b7ac37a2189d27d6a4afaf4031951ee
unknown <anastasiatevzaia@gmail.com>
2024-02-17 22:00:12 +0400

    Updated main.py

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
+from abc import ABC, abstractmethod
+class Vehicle(ABC):
+    @abstractmethod
+    def start_engine(self):
+        pass
+    @abstractmethod
+    def stop_engine(self):
+        pass
+class Car(Vehicle):
+    def __init__(self, max_speed, current_speed):
+        self.max_speed = max_speed
+        self.current_speed = current_speed
+        self.engine_started = False
+    def start_engine(self):
+        if not self.engine_started:
+            self.engine_started = True
+            return "Car started"
+        else:
+            return "Car is alrrady running"
+    def stop_engine(self):
+        if self.engine_started:
+            self.engine_started = False
+            return "Car stopped"
+        else:
+            return "Car iis already stopped"
 
-class Heart:
-    def __init__(self, output):
-        self.output = output
-
-    def health_status(self):
-        return "High blood pressure" if self.output > 70\
-            else "Feeling good"
-
-
-class Brain:
-    def __init__(self, load):
-        self.load = load
-
-    def brain_state(self):
-        return "Tired" if self.load > 90 \
-            else "Rested"
-class Leg:
-    def __init__(self, speed):
-        self.speed = speed
-
-    def motion_status(self):
-            if self.speed > 10:
-                return "Running"
-            elif 0 < self.speed <= 10:
-                return "Walking"
-            else:
-                return "Standing"
-
-class Person:
-    def __init__(self, heart_output, brain_load, leg_speed):
-                self.heart = Heart(heart_output)
-                self.brain = Brain(brain_load)
-                self.leg = Leg(leg_speed)
+class SportCar(Car):
+    def __init__(self,max_speed, current_speed):
+        super().__init__(max_speed, current_speed)
+    def start_engine(self):
+        parent_start = super().start_engine()
+        return f"{parent_start} max speed: {self.max_speed}"
+    def stop_engine(self):
+        parent_stop = super().stop_engine()
+        self.current_speed = 0
+        return f"{parent_stop} current speed: {self.current_speed}"
 
 
-person_instance = Person(heart_output=75, brain_load=85, leg_speed=8)
+
 
